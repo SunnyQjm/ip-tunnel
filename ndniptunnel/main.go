bf732a15a37d660e9879dc1696058f26e2de8f0b
SunnyQjm <qjm253@pku.edu.cn>
2021-09-26 17:05:05 +0800

    fix

--- ndniptunnel/main.go
+++ ndniptunnel/main.go
@@ -51,8 +51,12 @@ func GoOnInterest(cstr *C.char) {
 	name := C.GoString(cstr)
 	common.LogWarn("onInterest: ", name)
 	// 接收到兴趣包，从缓存中取出一个IP包，封装到一个Data中发出
-	ipPacket := <-pktChan
-	sendData(ipPacket.RawPackets, name)
+	if len(pktChan) > 0 {
+		ipPacket := <-pktChan
+		sendData(ipPacket.RawPackets, name)
+	} else {
+		sendData(nil, name)
+	}
 }
 
 //export GoOnNack
